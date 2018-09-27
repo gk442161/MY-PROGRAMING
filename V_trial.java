7bc77a2e3edb41aaa70f8a8c0b1958d9469cca50
gaurav kumar <32134800+gk442161@users.noreply.github.com>
2018-09-27 15:42:22 +0530

    Update V_trial.java

--- V_trial.java
+++ V_trial.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class V_trial{
+class V_trial{                                      //INITIALIZATION OF VECTOR
 public static void main(String[] args)
 {int value,i,count=0;
  Vector v1=new Vector();
@@ -8,14 +8,14 @@ public static void main(String[] args)
  Integer n;
  Scanner sc=new Scanner(System.in);
  System.out.println("ENTER THE ELEMENTS");
- for(i=0;i<10;i++)
+ for(i=0;i<10;i++)                                //TAKING VALUES INTO VECTOR
  {
 	 value=sc.nextInt();
 	 v1.addElement(new Integer(value));
  }
  
 System.out.println("\nELEMENTS COPIED IN ARRAY"); 
-v1.copyInto(a);
+v1.copyInto(a);                        //COPY OF VECTOR INTO ARRAY
  for(i=0;i<10;i++)
  {
 	 System.out.println(a[i]);
@@ -23,7 +23,7 @@ v1.copyInto(a);
 
 System.out.println("\nENETR THE NUMBER FROM THE LIST TO FIND FREQUENCY OF IT");
  n=sc.nextInt(); 
-Enumeration en=v1.elements();
+Enumeration en=v1.elements();                       // FINDING FREQUENCY
     while(en.hasMoreElements())
 	{if(n.equals(en.nextElement()))
 		count++;
