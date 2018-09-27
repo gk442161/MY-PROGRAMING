9e9d7549052721d25f43d54e942c83d137a91543
gaurav kumar <32134800+gk442161@users.noreply.github.com>
2018-09-27 15:35:15 +0530

    Add files via upload

---
+++ V_trial.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class V_trial{
+public static void main(String[] args)
+{int value,i,count=0;
+ Vector v1=new Vector();
+ Integer a[]= new Integer[10];
+ Integer n;
+ Scanner sc=new Scanner(System.in);
+ System.out.println("ENTER THE ELEMENTS");
+ for(i=0;i<10;i++)
+ {
+	 value=sc.nextInt();
+	 v1.addElement(new Integer(value));
+ }
+ 
+System.out.println("\nELEMENTS COPIED IN ARRAY"); 
+v1.copyInto(a);
+ for(i=0;i<10;i++)
+ {
+	 System.out.println(a[i]);
+ }
+
+System.out.println("\nENETR THE NUMBER FROM THE LIST TO FIND FREQUENCY OF IT");
+ n=sc.nextInt(); 
+Enumeration en=v1.elements();
+    while(en.hasMoreElements())
+	{if(n.equals(en.nextElement()))
+		count++;
+	}
+       System.out.println("\tFREQUENCY OF "+n+" IS "+count);
+
+}
+}
+ 
